c4acd834157ac130e80230a5a3b79ea4148a03d3
Bruna Lombardi <bruna2lombardi@hotmail.com>
2021-04-23 22:59:25 -0300

    Revert "Pequenos ajustes nos formularios e nas views"
    This reverts commit 75c58444c30e922e26a9338370d52190f6d60aef.

--- TCC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TCC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace TCC.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Lembrar-me")]
+            [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
 
