29a340500e7bb43cf52d7a8735e23679e022a1f6
Lohan <lohan.massote@gmail.com>
2021-05-24 20:08:24 -0300

    alterações denuncia model

---
+++ TCC/Data/Migrations/20210524230639_ajustes denuncia.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TCC.Data.Migrations
+{
+    public partial class ajustesdenuncia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cpf",
+                table: "Denuncia",
+                type: "VARCHAR(11)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HorarioComGente",
+                table: "Denuncia",
+                type: "VARCHAR(100)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NomeDenunciado",
+                table: "Denuncia",
+                type: "VARCHAR(100)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TipoDenuncia",
+                table: "Denuncia",
+                type: "VARCHAR(100)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cpf",
+                table: "Denuncia");
+
+            migrationBuilder.DropColumn(
+                name: "HorarioComGente",
+                table: "Denuncia");
+
+            migrationBuilder.DropColumn(
+                name: "NomeDenunciado",
+                table: "Denuncia");
+
+            migrationBuilder.DropColumn(
+                name: "TipoDenuncia",
+                table: "Denuncia");
+        }
+    }
+}
--- TCC/Models/Denuncia.cs
+++ TCC/Models/Denuncia.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using TCC.Extensions;
 
 namespace TCC.Models
 {
@@ -13,6 +14,21 @@ namespace TCC.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        [CustomValidationCPF(ErrorMessage = "CPF inválido")]
+        [Column(TypeName = "VARCHAR(11)")]
+        public string Cpf { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string NomeDenunciado { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string HorarioComGente { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        public string TipoDenuncia { get; set; }
 
         [Required(ErrorMessage = "{0} - Campo requerido")]
         [Column(TypeName = "VARCHAR(500)")]
