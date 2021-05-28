34eb80fa8027db94c0d06f2dece7d33d47f4cf02
Vinicius Murta <vinimurta4740@gmail.com>
2021-05-28 17:49:02 -0300

    Adiciona model Horários

--- TCC/Models/Agendamento.cs
+++ TCC/Models/Agendamento.cs
@@ -19,7 +19,7 @@ namespace TCC.Models
         public AgendaCastramovel AgendaCastramovel { get; set; }
         public int AgendaCastramovelId { get; set; }
         [Column(TypeName = "VARCHAR(5)")]
-        public string Horario { get; set; }
+        public Horarios Horario { get; set; }
         [Required]
         [Column(TypeName = "VARCHAR(100)")]
         public string Status { get; set; }
---
+++ TCC/Models/Horarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TCC.Models
+{
+    [Table("Horarios")]
+    public class Horarios
+    {
+        [Key]
+        public int Id { get; set; }
+        public Boolean Disponivel { get; set; }
+        [Column(TypeName = "VARCHAR(5)")]
+        public string Horario { get; set; }
+        [Column(TypeName = "DATE")]
+        public DateTime Dia { get; set; }
+
+    }
+}
