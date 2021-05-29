ac21492821b40558421ae7a0ec30407e72eb45b1
Vinicius Murta <vinimurta4740@gmail.com>
2021-05-29 15:25:37 -0300

    Cria Horários

--- TCC/Controllers/HorariosController.cs
+++ TCC/Controllers/HorariosController.cs
@@ -22,6 +22,43 @@ namespace TCC.Controllers
             return View();
         }
 
+        public void Cadastrar(AgendaCastramovel model)
+        {
+            Horarios hora = new Horarios();
+            hora.Dia = model.Data;
+            hora.Id = 0;
+            hora.Disponivel = true;
+            int con = 1;
+            int time = 8;
+
+            for (int cont = 0; cont <= 17; cont++)
+            {
+                if(time != 12)
+                {
+                    if ((con % 2) == 0)
+                    {
+                        hora.Horario = time + ":30";
+                        time++;
+                    }
+                    else
+                    {
+                        hora.Horario = time + ":00";
+                    }
+
+                }else
+                {
+                    time++;
+                    con++;
+                }
+
+                _context.Horarios.Add(hora);
+
+                con++;
+            }
+
+            _context.SaveChanges();
+
+        }
         public Boolean verificaHorario(Agendamento model)
         {
             List<Horarios> horarios = _context.Horarios.ToList();
